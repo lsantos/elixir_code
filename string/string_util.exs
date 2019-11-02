b281439e0c03f1075bd299f14d0d93bd352fef95
Leandro Santos <leandro.bueno@globo.com>
2019-11-02 19:18:49 -0400

    Remove unnecessary map call

--- string/string_util.exs
+++ string/string_util.exs
@@ -3,8 +3,7 @@ defmodule StringUtil do
     def anagram(word1, word2)
     when is_list(word1) and is_list(word2) do
         word1
-        |> Enum.map(&_contains?(word2, &1))
-    	|> Enum.all?    
+    	|> Enum.all?(&_contains?(word2, &1))    
     end
    
     defp _contains?(words, word), do: Enum.any?(words, &(&1 == word))
